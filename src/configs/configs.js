cff62fe6f360f72d9370e9f3f3d65141a3521d8d
André <andrepedrobotelho@gmail.com>
2021-08-01 01:00:13 -0300

    refactor : add env variables

--- src/configs/configs.js
+++ src/configs/configs.js
@@ -1,9 +1,8 @@
 const { Client } = require('@elastic/elasticsearch')
 const axios = require('axios');
-const urlBase = 'http://elasticsearch:9200'
 
 const client = new Client({
-  node: urlBase,
+  node: process.env.URL_ELASTICSEARCH,
   apiVersion: '7.2', // use the same version of your Elasticsearch instance
 });
 
@@ -13,28 +12,22 @@ const checkAndCreateIndice = function (index) {
   }, async (err, res, status) => {
 
     if (!res.body) {
-      client.indices.create({
+      await client.indices.create({
         index
-      }, async (err, res, status) => {
-        if (err) {
-          return console.log(err)
-        }
-
-        await axios.put(`${urlBase}/_ingest/pipeline/attachment`, {
-          "description": "new pipeline",
-          "processors": [
-            {
-              "attachment": {
-                "field": "data"
-              }
-            }
-          ]
-        });
-
-        console.log('Index create with success');
       })
 
+      await axios.put(`${process.env.URL_ELASTICSEARCH}/_ingest/pipeline/attachment`, {
+        "description": "new pipeline",
+        "processors": [
+          {
+            "attachment": {
+              "field": "data"
+            }
+          }
+        ]
+      });
 
+      console.log('Index create with success');
     }
   })
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+require('dotenv').config()
+
 const express = require('express')
 const routes = require('./routes')
+
 const { checkAndCreateIndice } = require('./configs/configs')
 
 require('./watches')
 
 const app = express()
-const port = 7000
+const port = process.env.PORT
 
 checkAndCreateIndice('english')
 
 app.use('/', routes)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Project listening on port ${port}!`))
